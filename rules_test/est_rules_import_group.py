e66dea73b09030658b7ed1f8a3f2a054802633ea
imustafin <ilyasmustafin@imot.io>
2023-07-20 15:29:38 +0300

    add var for import

--- rules_test/est_rules_import_group.py
+++ rules_test/est_rules_import_group.py
@@ -1,6 +1,14 @@
 from playwright.sync_api import Page, expect
 from utils.variables import *
 
+'''
+1 login admin
+2 choose user in bar
+3 go to rules
+4 click import rules
+5 choose user
+6 choose rules
+7'''
 def test_example(page: Page) -> None:
     page.goto(URL)
     '''login'''
@@ -9,5 +17,7 @@ def test_example(page: Page) -> None:
     page.locator("[id='mui-3']").click()
 
     page.get_by_role("link", name="Разметка").click()
-    page.get_by_role("button", name="Импортировать правила").click()
+    page.locator("#react-select-2-input").fill("import")
+
+    #page.get_by_role("button", name="Импортировать правила").click()
     
--- utils/variables.py
+++ utils/variables.py
@@ -5,10 +5,15 @@ USER ="1userIM"
 OPERATOR ="2operatorIM"
 MANAGER = "3managerIM"
 ADMIN = "4adminIM"
+USER_FOR_IMPORT = "importFrom"
 PASSWORD = "Qaz123wsX"
 
 LOGIN_NEGOTIVE = "1userI"
 PASSWORD_NEGOTIVE ="Qaz123ws"
+
+#import rules and dict
+FROM_USER_IMPORT = "importFrom"
+TO_USER_IMPORT = "importTo"
 
 REGISTRATION_NAME = "John Coltrane"
 REGISTRATION_EMAIL = "ilyasmustafin@imot.io"
