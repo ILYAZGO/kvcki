f5789a6eecee3b2c8e59093f43690d93976a4414
i.mustafin <ilyasmustafin@imot.io>
2023-08-24 13:20:34 +0300

    add timeout

--- rules_test/test_rules_add_rule_outside_group.py
+++ rules_test/test_rules_add_rule_outside_group.py
@@ -22,7 +22,7 @@ def test_example(page: Page) -> None:
 
     '''create group'''
     page.get_by_test_id(BUTTON_DOBAVIT_TEG).click()
-    page.get_by_test_id(INPUT_NAZVANIE_TEGA).type("66666")
+    page.get_by_test_id(INPUT_NAZVANIE_TEGA).type("66666", timeout=wait_until_visible)
     page.keyboard.press('Enter')  # kostil'
 
     time.sleep(3)
@@ -31,11 +31,10 @@ def test_example(page: Page) -> None:
     expect(page.locator(NAZVANIE_PRAVILA_TEGIROVANIYA)).to_have_value("66666", timeout=wait_until_visible)  # check rule
     # expect(page.get_by_text("Неотсортированные")).to_have_count(count=2)  # po4emu to valitsa
     ''''teardown'''
-    page.locator(".css-izdlur").click()
-    page.get_by_text("Удалить", exact=True).click()
-    page.get_by_role("button", name="Удалить").click()
-    time.sleep(2)
-    page.locator(BUTTON_KORZINA).click()
+    page.locator(".css-izdlur").click(timeout=wait_until_visible)
+    page.get_by_text("Удалить", exact=True).click(timeout=wait_until_visible)
+    page.get_by_role("button", name="Удалить").click(timeout=wait_until_visible)
+    page.locator(BUTTON_KORZINA).click(timeout=wait_until_visible)
     '''check teardown'''
     expect(page.get_by_text("Неотсортированные")).not_to_be_visible(timeout=wait_until_visible)
 
