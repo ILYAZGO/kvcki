87e3bd30d6834181011c04d710c43ade8b4fff04
i.mustafin <ilyasmustafin@imot.io>
2023-10-20 13:57:46 +0300

    add test notifications api

--- notifications/test_notifications_api.py
+++ notifications/test_notifications_api.py
@@ -13,23 +13,27 @@ def test_example(page: Page) -> None:
     '''login'''
     auth(USER_FOR_IMPORT, PASSWORD, page)
 
-    add_notification("Email", page)
+    add_notification("API", page)
 
     #  fill name
     page.locator('[placeholder="Например: Жалоба на сотрудника"]').fill("auto-test-email")
+    #  fill url
+    page.locator('[placeholder="шаблон URL"]').fill(URL)
+    #  send again when rull changed
+    page.locator('[type="checkbox"]').nth(0).click()
 
     add_filter("По тегам", "22222", page)
 
     fill_message("someText ", page)
 
-    fill_attr_for_email('letterTheme', 'mail@.mail.com', page)
-
 
     page.locator('[class="styles_buttonsGroup__aLY1I"]').get_by_role("button").nth(0).click()
 
+    page.locator('[placeholder="Можно проставить авторизацию и content-type"]').fill("someHeaders")
+
     page.wait_for_selector(BUTTON_KORZINA)
 
-    page.locator('[type="checkbox"]').first.click()
+    page.locator('[type="checkbox"]').nth(0).click()
 
     page.locator(BUTTON_KORZINA).click()
 
--- pages/notifications.py
+++ pages/notifications.py
@@ -3,4 +3,37 @@ BUTTON_OPOVESHENIA = '[href*="/notifications"]'
 
 BUTTON_KORZINA = '[aria-label="Удалить"]'
 
-#INPUT_
+INPUT_LETTER_THEME = '[placeholder="Укажите тему письма"]'
+INPUT_EMAIL = '[placeholder="example@mail.com"]'
+
+
+
+def add_filter(filterType, filterName, page="page: Page"):
+    #  dobavit filtr
+    page.locator('[style="color: rgb(146, 84, 222);"]').click()
+    #  choose po tegam
+    page.locator(".css-woue3h-menu").get_by_text(filterType, exact=True).nth(1).click()
+    #  tupo click
+    page.locator(".styles_title__nLZ-h").click()
+    #  fill filter
+    page.locator('[autocorrect=off]').nth(0).fill(filterName)
+    page.wait_for_timeout(3000)
+    #  choose filter
+    page.get_by_text(filterName, exact=True).nth(0).click()
+
+def add_notification(notificationType, page="page: Page"):
+    page.locator(BUTTON_OPOVESHENIA).click()
+    #  add new
+    page.locator(".styles_addNewRule__zhZVC").get_by_role("button").click()
+    #  click to list
+    page.locator('[class="css-8mmkcg"]').first.click()
+    #  choose email
+    page.locator('[class=" css-164zrm5-menu"]').get_by_text(notificationType, exact=True).click()
+
+def fill_attr_for_email(letterTheme, email, page="page: Page"):
+    page.locator(INPUT_LETTER_THEME).fill(letterTheme)
+    page.locator(INPUT_EMAIL).fill(email)
+
+def fill_message(text, page="page: Page"):
+    page.locator('[class="styles_textarea__+sldQ"]').fill(text)
+    page.locator('[aria-label="ID звонка. Пример: 123456789012345678901234"]').click()
