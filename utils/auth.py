ae19d6b6a74db87d61e94e3f39fba07decd2c7b7
i.mustafin <ilyasmustafin@imot.io>
2023-11-03 12:16:42 +0300

    fix locator\selector

--- utils/auth.py
+++ utils/auth.py
@@ -1,7 +1,7 @@
 def auth(login, password, page="page: Page"):
-    page.wait_for_locator("[id='mui-1']")
+    page.wait_for_selector("[id='mui-1']")
     page.locator("[id='mui-1']").fill(login)
-    page.wait_for_locator("[id='mui-2']")
+    page.wait_for_selector("[id='mui-2']")
     page.locator("[id='mui-2']").fill(password)
-    page.wait_for_locator("[id='mui-3']")
+    page.wait_for_selector("[id='mui-3']")
     page.locator("[id='mui-3']").click()
