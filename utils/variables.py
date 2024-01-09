f4d95c61485ddf6e9996b853d11c98256227799e
i.mustafin <ilyasmustafin@imot.io>
2024-01-09 11:16:49 +0300

    fix url

--- utils/variables.py
+++ utils/variables.py
@@ -3,7 +3,7 @@ from datetime import datetime
 
 #LIST OF URLS
 
-URL = "http://192.168.10.101/feature-dev-1513/"    #os.getenv('PUBLIC_URL') or "https://app.stand.imot.io/ru"
+URL = os.getenv('PUBLIC_URL') or "https://app.stand.imot.io/ru"
 API_URL = "https://api.stand.imot.io"
 
 # LIST OF TIMEOUTS
