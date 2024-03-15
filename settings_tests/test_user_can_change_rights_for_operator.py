f45c620ff50001ad17e4679b7d519b705c2fbf91
i.mustafin <ilyasmustafin@imot.io>
2024-03-15 16:56:39 +0300

    add tests for changing rights user oper

---
+++ settings_tests/test_user_can_change_rights_for_operator.py
@@ -0,0 +1,47 @@
+from playwright.sync_api import Page, expect
+from utils.variables import *
+from utils.auth import auth
+from pages.settings import *
+from utils.create_delete_user import create_user, delete_user, create_operator
+import pytest
+
+
+# check admin can change rights for user and operator
+@pytest.mark.independent
+@pytest.mark.settings
+def test_example(page: Page) -> None:
+    USER_ID_USER, TOKEN_USER, LOGIN_USER = create_user(API_URL, ROLE_USER, PASSWORD)
+    USER_ID_OPERATOR, TOKEN_OPERATOR, LOGIN_OPERATOR = create_operator(API_URL, USER_ID_USER, PASSWORD)
+
+    page.goto(URL, timeout=timeout)
+
+    auth(LOGIN_USER, PASSWORD, page)
+
+    # change rights for operator
+
+    click_settings(page)
+
+    page.locator(BUTTON_EMPLOYEES).click()
+    page.wait_for_selector(BUTTON_DOBAVIT_SOTRUDNIKA)
+
+    page.locator('[aria-rowindex="2"]').locator('[class="rs-table-cell rs-table-cell-first"]').click()
+    page.wait_for_timeout(500)
+    page.wait_for_selector(INPUT_LOGIN)
+
+    page.locator(BUTTON_RIGHTS).click()
+
+    expect(page.locator(BLOCK_ONE_RIGHT)).to_have_count(22)
+
+    click_all_checkboxes_on_page(page)
+
+    press_save_in_rights(page)
+
+    page.reload()
+    page.wait_for_selector(BUTTON_SAVE_IN_RIGHTS)
+
+    all_checkboxes_to_be_checked(page)
+
+    assert all_checkboxes_to_be_checked(page) == True
+
+    delete_user(API_URL, TOKEN_USER, USER_ID_USER)
+    delete_user(API_URL, TOKEN_OPERATOR, USER_ID_OPERATOR)
