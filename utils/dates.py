601d6ecdd4fc92ed81fa81144288bd27ce0bffb2
i.mustafin <ilyasmustafin@imot.io>
2024-04-01 13:40:48 +0300

    fuck dates

--- utils/dates.py
+++ utils/dates.py
@@ -2,6 +2,7 @@ from datetime import datetime, timedelta
 import calendar
 
 
+
 current_date = datetime.now()
 
 today = current_date.strftime("%d/%m/%Y")
@@ -13,17 +14,17 @@ first_day_week_ago = (current_date - timedelta(days=6)).strftime("%d/%m/%Y")
 
 days_in_current_month = calendar.monthrange(current_date.year, current_date.month)[1]
 
-#first_day_month_ago = (current_date - timedelta(days=30)).strftime("%d/%m/%Y")
+first_day_month_ago = (current_date - timedelta(days=30)).strftime("%d/%m/%Y") #(current_date + relativedelta(weeks=-4)).strftime("%d/%m/%Y")                   #(current_date - timedelta(days=30)).strftime("%d/%m/%Y")
+print(first_day_month_ago)
 
-
-if days_in_current_month == 31:
-    first_day_month_ago = (current_date - timedelta(days=28)).strftime("%d/%m/%Y")
-elif days_in_current_month == 30:
-    first_day_month_ago = (current_date - timedelta(days=30)).strftime("%d/%m/%Y")
-elif days_in_current_month == 29:
-    first_day_month_ago = (current_date - timedelta(days=30)).strftime("%d/%m/%Y")
-else:
-    first_day_month_ago = (current_date - timedelta(days=30)).strftime("%d/%m/%Y")
+#if days_in_current_month == 31:
+#    first_day_month_ago = (current_date - timedelta(days=28)).strftime("%d/%m/%Y")
+#elif days_in_current_month == 30:
+#    first_day_month_ago = (current_date - timedelta(days=30)).strftime("%d/%m/%Y")
+#elif days_in_current_month == 29:
+ #   first_day_month_ago = (current_date - timedelta(days=30)).strftime("%d/%m/%Y")
+#else:
+ #   first_day_month_ago = (current_date - timedelta(days=30)).strftime("%d/%m/%Y")
 # ----------------------------------------------------------------------------------
 
 first_day_year_ago = (current_date - timedelta(days=365)).strftime("%d/%m/%Y")
