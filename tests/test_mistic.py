966dd956a1596ad0da9d43bd3c3eee3005426ff1
i.mustafin <ilyasmustafin@imot.io>
2024-09-09 16:30:41 +0300

    added test mistic

---
+++ tests/test_mistic.py
@@ -0,0 +1,146 @@
+from playwright.sync_api import Page, expect, BrowserContext
+from utils.variables import *
+from utils.auth import auth
+from pages.communications import *
+from utils.dates import *
+from datetime import datetime
+from utils.create_delete_user import create_user, delete_user
+import os
+import pytest
+import allure
+
+
+@pytest.mark.calls
+@pytest.mark.independent
+@allure.title("test_mistic_mayorov")
+@allure.severity(allure.severity_level.CRITICAL)
+@allure.description("test_mistic_mayorov")
+def test_mistic_mayorov(base_url, page: Page) -> None:
+    BUTTON_VOITI = "[type='submit']"
+
+    with allure.step("Go to url"):
+        page.goto(base_url, timeout=wait_until_visible)
+
+    with allure.step("Auth with 0AleberOper"):
+        auth("0AleberOper", PASSWORD, page)
+        page.wait_for_selector(FIRST_DATE)
+
+    with allure.step("Choose period"):
+        page.wait_for_selector(INPUT_PO_TEGAM, timeout=wait_until_visible)
+        page.locator(FIRST_DATE).click()
+        page.wait_for_timeout(100)
+        page.locator(FIRST_DATE).fill("03/09/2024")
+        page.wait_for_timeout(300)
+        page.locator(LAST_DATE).click()
+        page.wait_for_timeout(100)
+        page.locator(LAST_DATE).fill("09/09/2024")
+        page.wait_for_timeout(300)
+        page.keyboard.press("Enter")
+        page.wait_for_timeout(300)
+
+    with allure.step("Search coomunications"):
+        press_find_communications(page)
+
+    with allure.step("Check"):
+        expect(page.locator(NAYDENO_ZVONKOV).nth(0)).to_have_text("Найдено коммуникаций 999 из 999", timeout=wait_until_visible)
+
+    with allure.step("Quit from account"):
+        page.locator('[aria-label="Профиль"]').get_by_role("button").click()
+        page.get_by_text("Выйти", exact=True).click()
+
+    with allure.step("Check that quit was successful"):
+        expect(page.locator(BUTTON_VOITI)).to_be_visible()
+
+    with allure.step("Auth with 1AleberOper"):
+        auth("1AleberOper", PASSWORD, page)
+        page.wait_for_selector(FIRST_DATE)
+
+    with allure.step("Choose period"):
+        page.wait_for_selector(INPUT_PO_TEGAM, timeout=wait_until_visible)
+        page.locator(FIRST_DATE).click()
+        page.wait_for_timeout(100)
+        page.locator(FIRST_DATE).fill("03/09/2024")
+        page.wait_for_timeout(300)
+        page.locator(LAST_DATE).click()
+        page.wait_for_timeout(100)
+        page.locator(LAST_DATE).fill("09/09/2024")
+        page.wait_for_timeout(300)
+        page.keyboard.press("Enter")
+        page.wait_for_timeout(300)
+
+    with allure.step("Search coomunications"):
+        press_find_communications(page)
+
+    with allure.step("Check"):
+        expect(page.locator(NAYDENO_ZVONKOV).nth(0)).to_have_text("Найдено коммуникаций 776 из 776",
+                                                                  timeout=wait_until_visible)
+
+    with allure.step("Quit from account"):
+        page.locator('[aria-label="Профиль"]').get_by_role("button").click()
+        page.get_by_text("Выйти", exact=True).click()
+
+    with allure.step("Check that quit was successful"):
+        expect(page.locator(BUTTON_VOITI)).to_be_visible()
+
+    with allure.step("Auth with 2AleberOper"):
+        auth("2AleberOper", PASSWORD, page)
+        page.wait_for_selector(FIRST_DATE)
+
+    with allure.step("Choose period"):
+        page.wait_for_selector(INPUT_PO_TEGAM, timeout=wait_until_visible)
+        page.locator(FIRST_DATE).click()
+        page.wait_for_timeout(100)
+        page.locator(FIRST_DATE).fill("03/09/2024")
+        page.wait_for_timeout(300)
+        page.locator(LAST_DATE).click()
+        page.wait_for_timeout(100)
+        page.locator(LAST_DATE).fill("09/09/2024")
+        page.wait_for_timeout(300)
+        page.keyboard.press("Enter")
+        page.wait_for_timeout(300)
+
+    with allure.step("Search coomunications"):
+        press_find_communications(page)
+
+    with allure.step("Check"):
+        expect(page.locator(NAYDENO_ZVONKOV).nth(0)).to_have_text("Найдено коммуникаций 181 из 181",
+                                                                  timeout=wait_until_visible)
+
+    with allure.step("Quit from account"):
+        page.locator('[aria-label="Профиль"]').get_by_role("button").click()
+        page.get_by_text("Выйти", exact=True).click()
+
+    with allure.step("Check that quit was successful"):
+        expect(page.locator(BUTTON_VOITI)).to_be_visible()
+
+
+    with allure.step("Auth with 3AleberOper"):
+        auth("3AleberOper", PASSWORD, page)
+        page.wait_for_selector(FIRST_DATE)
+
+    with allure.step("Choose period"):
+        page.wait_for_selector(INPUT_PO_TEGAM, timeout=wait_until_visible)
+        page.locator(FIRST_DATE).click()
+        page.wait_for_timeout(100)
+        page.locator(FIRST_DATE).fill("03/09/2024")
+        page.wait_for_timeout(300)
+        page.locator(LAST_DATE).click()
+        page.wait_for_timeout(100)
+        page.locator(LAST_DATE).fill("09/09/2024")
+        page.wait_for_timeout(300)
+        page.keyboard.press("Enter")
+        page.wait_for_timeout(300)
+
+    with allure.step("Search coomunications"):
+        press_find_communications(page)
+
+    with allure.step("Check"):
+        expect(page.locator(NAYDENO_ZVONKOV).nth(0)).to_have_text("Найдено коммуникаций 441 из 441",
+                                                                  timeout=wait_until_visible)
+
+    with allure.step("Quit from account"):
+        page.locator('[aria-label="Профиль"]').get_by_role("button").click()
+        page.get_by_text("Выйти", exact=True).click()
+
+    with allure.step("Check that quit was successful"):
+        expect(page.locator(BUTTON_VOITI)).to_be_visible()
