01ef544a9e7ca3fb5b38ad8a15389e91f1cb1dbe
i.mustafin <ilyasmustafin@imot.io>
2025-02-13 14:31:02 +0300

    add call

---
+++ send_file2.py
@@ -0,0 +1,115 @@
+import os
+import json
+import requests
+from datetime import datetime
+import random
+
+
+IMOT_TOKEN = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiNjdhZGE4ZTc5MzdkZjdmNGVhMjU1MWZkIiwiZXhwIjoxNzM5ODY2MzMyfQ.WlMHwcHL-xiwf5GVyRB1CanoBCQuh4SIn7hWTIEi4O4' # нужно вставить сюда API ключ.
+UPLOAD_ENDPOINT = 'https://api.stand.imot.io/call/'
+CHECK_ENDPOINT = 'https://api.stand.imot.io/find_call'
+unique_id= f"2ceb2380bahg63d{random.randint(100000,999999)}a96"
+
+def upload_call_to_imotio(
+        token: str,
+        unique_id: str,          # ID звонка в системе клиента,
+                                 #   IMOT.IO проверяет уникальность этого поля при загрузке
+        call_time: datetime,     # время совершения звонка
+        filename: str,           # имя файла который нужно загрузить
+        client_channel: int = 0, # номер канала в котором говорит клиент, для стерео файла,
+                                 #    если клиент в правом ухе нужно передавать 1
+        client_phone: str = '',  # номер телефона клиента
+        operator_phone: str = '',# номер телефона сотрудника
+        meta_data: dict = None   # Мета данные о звонке. Произвольный набор данных,
+                                 #    в IMOT.IO отображаются как теги звонка
+) -> str:
+    """
+    Выполняет загрузку файла в сервис IMOT.IO
+    Возвращает ID звонка в сервисе
+    При повторном вызове для сушествующего звонка функия возврашает сушествующий ID звонка.
+    Если загрузка по какой-то причине не удалась, функция кидает исключения.
+    """
+
+    if not unique_id:
+        raise ValueError("unique_id required")
+
+    if not IMOT_TOKEN:
+        raise ValueError("Fill value IMOT_TOKEN to make requests")
+
+    headers = {'Authorization': token}
+
+    req = requests.get(f"{CHECK_ENDPOINT}/{unique_id}", headers=headers)
+    if req.status_code == 200:
+        # Звонок уже существует
+        response = req.json()
+        if 'call_id' not in response:
+            raise RuntimeError(f"{CHECK_ENDPOINT} don't return call_id. Response: {response}")
+
+        return response['call_id']
+
+        # update params
+    elif req.status_code != 404:
+        # другой ответ от imot.io который неожиданен для нас.
+        req.raise_for_status()
+
+    # мы дошли до сюда после того как IMOT.IO адекватно ответил, что такого звонка у него нет
+
+    # Define the correct path to the file
+    filename = os.path.join(os.path.dirname(os.path.abspath(__file__)), "audio/stereo.opus")
+
+    # Ensure the path is absolute
+    filename = os.path.abspath(filename)
+
+    if not os.path.isfile(filename):
+        raise ValueError(f"{filename=} is not valid file")
+
+    files = {"stereo_audio": (os.path.basename(filename), open(filename, 'rb'))}
+
+    tags = []
+    if meta_data:
+        for k,v in meta_data.items():
+            if v:
+                tags.append({"name": str(k), "value": str(v)})
+            else:
+                tags.append({"name": str(k)})
+
+
+    params = {'call_time': call_time.timestamp(),
+              'client_phone': client_phone,
+              'operator_phone': operator_phone,
+              'client_channel': client_channel,
+              'unique_id': unique_id,
+              'tags': tags,
+    }
+
+    req = requests.post(
+            UPLOAD_ENDPOINT,
+            files=files,
+            data={'params': json.dumps(params)},
+            headers=headers
+    )
+    req.raise_for_status()
+    response = req.json()
+    return response
+
+if __name__ == '__main__':
+    try:
+        call_id = upload_call_to_imotio(
+                token=IMOT_TOKEN,
+                unique_id=unique_id,
+                call_time = datetime(2025, 2, 10, 13, 15, 0),
+                filename='stereo.opus',
+                client_phone = '+79031112233',
+                operator_phone = '102',
+                meta_data = {'Сотрудник': 'Вася',
+                             'Отдел': 'Продажи',
+                             'Направление звонка': 'Входящий',
+                             'ID сотрудника': 123,
+                             'Холодный обзвон': '',  # значение может быть пустым,
+                                                     # это превратится в тег без значения
+                            })
+        print(f"Call uploaded: {call_id}")
+    except Exception as e:
+        print(f"Failed to upload: {e}")
+        import traceback
+        traceback.print_exc()
--- utils/create_delete_user.py
+++ utils/create_delete_user.py
@@ -3,6 +3,7 @@ import random
 import json
 import os
 import time
+from send_file2 import *
 from datetime import datetime, timedelta, timezone
 from loguru import logger
 
@@ -39,8 +40,14 @@ def create_user(URL, ROLE, PASSWORD):
         'name': NAME,
         'password': PASSWORD,
         "sttOptions": {
-            "sttEconomize": False,
-            "sttOptionsMap": {}
+            "sttEngine":"nlab_speech",
+            "sttEconomize":False,
+            "sttOptionsMap":
+                {"language":"ru-RU",
+                 "model":"general",
+                 "merge_all_to_one_audio":False,
+                 "count_per_iteration":1,
+                 "diarization":False}
         }
     }
 
@@ -96,103 +103,34 @@ def create_user(URL, ROLE, PASSWORD):
         }
 
         # upload call
-        client_audio_path = os.path.join('audio', 'count-in.opus')
-        operator_audio_path = os.path.join('audio', 'count-out.opus')
-
-        # time for call
+        _unique_id = f"2ceb2380bahg54d{random.randint(100000, 999999)}a96"
         current_time = datetime.now(timezone.utc)
-        delta_ten = current_time + timedelta(minutes=10)
-        # delta_one = current_time + timedelta(minutes=1)
-        now = current_time.strftime('%Y-%m-%dT%H:%M:%S.%f')[:-3] + 'Z'
-        # one_min_later = delta_one.strftime('%Y-%m-%dT%H:%M:%S.%f')[:-3] + 'Z'
-        ten_min_later = delta_ten.strftime('%Y-%m-%dT%H:%M:%S.%f')[:-3] + 'Z'
 
-        headers_for_upload = {
-            'accept': 'application/json',
-            'Authorization': token_for_user,
-        }
+        _call_id = upload_call_to_imotio(
+            token=token_for_user,
+            unique_id=_unique_id,
+            call_time=datetime(
+                current_time.year,
+                current_time.month,
+                current_time.day,
+                current_time.hour,
+                current_time.minute,
+                current_time.second
+            ),
+            filename='stereo.opus',
+            client_phone='0987654321',
+            operator_phone='1234567890',
+            meta_data={'auto': 'test',
+                       #'ID сотрудника': 123,
+                       'upload': '',  # значение может быть пустым, это превратится в тег без значения
+                       })
 
-        # json
-        json_data = {
-            # "operator_filename": "string",
-            # "bitrix_deal_id": "string",
-            # "telegram_chat_id": "string",
-            # "bitrix_crm_phone_number": "string",
-            # "stereo_url": "string",
-            # "speaker_names": ["first", "second"],
-            "unique_id": f"2ceb2380baed63d{random.randint(100000, 999999)}a96",
-            # "bitrix_crm_entity_type": "string",
-            # "bitrix_crm_entity_id": "string",
-            # "bitrix_entity_type": "string",
-            # "amo_note_id": "string",
-            # "hangup": "client",
-            "call_time": now,
-            # "stereo_audio_md5": "string",
-            # "answer_time": one_min_later ,
-            "client_channel": 1,
-            "has_audio": True,
-            # "telegram_message_id": "string",
-            # "bitrix_call_id": "string",
-            "end_time": ten_min_later,
-            # "amo_url_md5": "string",
-            "operator_phone": "1234567890",
-            # "operator_url": "string",
-            "integration_data": {
-                "integration_id": f"63ac6380baed63d{random.randint(100000, 999999)}a96",
-                "task_id": f"2c0bfb31-2596-49a3-8a92-19a93dbc078f",
-                "service_name": "auto_test"
-            },
-            "is_only_new_tags": False,
-            # "crm_entity_id": "string",
-            "unanswered": False,
-            # "conversation_id": f"{random.randint(100000,999999)}",
-            "is_mono": False,
-            # "language": "ru",
-            "tags": [
-                {
-                    "tag_type": "upload",
-                    "name": "auto",
-                    "value": "test",
-                    "visible": True
-                }
-            ],
-            "client_phone": "0987654321",
-            # "multichannel_params": [
-            #     {
-            #         "speaker_name": "Alex",
-            #         "is_operator": False,
-            #         "unique_id": f"{random.randint(100000,999999)}"
-            #     }
-            # ],
-            # "client_url": "string",
-            # "client_filename": "string",
-            # "amo_event_id": "string",
-            "is_multichannel": False,
-            "direction": "income"
-        }
-
-        # move json to string
-        json_str = json.dumps(json_data)
-
-        params_for_upload = {'params': (None, json_str)}
-
-        upload = requests.post(url=URL + "/call/", headers=headers_for_upload, data=params_for_upload,
-                               files=
-                               {
-                                   'client_audio': ('count-in.opus',
-                                                    open(client_audio_path, 'rb'),
-                                                    'audio/ogg'),
-                                   'operator_audio': ('count-out.opus',
-                                                      open(operator_audio_path, 'rb'),
-                                                      'audio/ogg')
-                               })
-
-        if upload.status_code == 200:
-            logger.opt(depth=1).info(f"\n>>>>> AUDIO id:{upload.text} uploaded  {URL}/call/ <<<<<")
+        if len(_call_id) == 24:
+            logger.opt(depth=1).info(f"\n>>>>> AUDIO id: {_call_id} uploaded to {URL} <<<<<")
         else:
-            logger.opt(depth=1).info(f"\n>>>>> AUDIO upload error {upload.status_code} text {upload.text} <<<<<")
+            logger.opt(depth=1).info(f"\n>>>>> AUDIO upload error {_call_id} text  <<<<<")
 
-        ##create groups
+        #create groups
         rule_group = {
             "title": "auto_rule_group",
             "enabled": True
