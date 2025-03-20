9039c22defc5cc9d85a9d611b954ca4e89dfc01d
i.mustafin <ilyasmustafin@imot.io>
2025-03-20 21:07:04 +0300

    check stand5

--- tests/test_page_requests.py
+++ tests/test_page_requests.py
@@ -1,10 +1,7 @@
 from playwright.sync_api import Page, expect, BrowserContext, sync_playwright, Playwright
 from utils.variables import *
 from pages.communications import *
-from utils.dates import *
-from datetime import datetime
 from utils.create_delete_user import create_user, delete_user, give_access_right, give_users_to_manager
-import os
 import pytest
 import allure
 
@@ -13,20 +10,18 @@ import allure
 @allure.title("test_experiment")
 @allure.severity(allure.severity_level.NORMAL)
 @allure.description("test_experiment")
-def est_experiment(base_url, page: Page) -> None:
+def test_experiment(base_url, page: Page) -> None:
     communications = Communications(page)
 
     # Intercept and store requests
     captured_requests = []
 
-
-
     with allure.step("Create user"):
         USER_ID, TOKEN, LOGIN = create_user(API_URL, ROLE_USER, PASSWORD)
 
     with allure.step("Go to url"):
-        communications.navigate(base_url)
-        page.wait_for_timeout(5000)
+        communications.navigate("http://192.168.10.101/feature-dev-3409/")
+        page.wait_for_timeout(2000)
 
     page.on("request", lambda request: (
         captured_requests.append({
@@ -34,14 +29,28 @@ def est_experiment(base_url, page: Page) -> None:
             'method': request.method,
             # 'headers': request.headers,
             # 'post_data': request.post_data
-        }) if request.resource_type == 'xhr' else None
+        }) if request.resource_type in ['xhr', 'fetch'] else None
     ))
 
     with allure.step("Auth with user"):
         communications.auth(LOGIN, PASSWORD)
 
-    page.wait_for_timeout(5000)
-    assert captured_requests == []
+    page.wait_for_timeout(7000)
+    assert len(captured_requests) == 24
+    assert sum('/token' in entry.get('url', '') for entry in captured_requests) == 1
+    assert sum('/user/me/access_rights' in entry.get('url', '') for entry in captured_requests) == 2
+    assert sum('/search_criterias/default_keys' in entry.get('url', '') for entry in captured_requests) == 2
+    assert sum('/search_filters/' in entry.get('url', '') for entry in captured_requests) == 2
+    assert sum('/user/me?with_quota=true' in entry.get('url', '') for entry in captured_requests) == 3
+    assert sum('/reports' in entry.get('url', '') for entry in captured_requests) == 2
+    assert sum('/translation/get_all_languages' in entry.get('url', '') for entry in captured_requests) == 1
+    assert sum('/get_date_range_by_period?period=today' in entry.get('url', '') for entry in captured_requests) == 1
+    assert sum('/users/?with_childs=true' in entry.get('url', '') for entry in captured_requests) == 2
+    assert sum('/open_id/list' in entry.get('url', '') for entry in captured_requests) == 1
+    assert sum('/user/filter?with_quota=true' in entry.get('url', '') for entry in captured_requests) == 2
+    assert sum('/search_criterias/' in entry.get('url', '') for entry in captured_requests) == 4 # 2 here 2 with /default_keys
+    assert sum('/search_calls/' in entry.get('url', '') for entry in captured_requests) == 2
+    assert sum(f'/user/{USER_ID}?with_quota=true' in entry.get('url', '') for entry in captured_requests) == 1
 
     with allure.step("Delete user"):
         delete_user(API_URL, TOKEN, USER_ID)
