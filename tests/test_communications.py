11ebf2e87ac4a9e763a8e69eafa682c3c6f16d83
i.mustafin <ilyasmustafin@imot.io>
2025-04-01 12:45:42 +0300

    added test2

--- tests/test_communications.py
+++ tests/test_communications.py
@@ -2387,7 +2387,7 @@ def test_open_other_pages_from_communications(base_url, page: Page, context: Bro
         USER_ID_USER, TOKEN_USER, LOGIN_USER = create_user(API_URL, ROLE_USER, PASSWORD)
 
     with allure.step("Go to url"):
-        communications.navigate("http://192.168.10.101/feature-dev-3452/")
+        communications.navigate(base_url)
 
     with allure.step("Auth with user"):
         communications.auth(LOGIN_ADMIN, PASSWORD)
